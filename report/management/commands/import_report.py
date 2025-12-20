36c144e64af5aa832d34c56bdd8206a4fdd6a9d9
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-12-20 11:48:45 +0530

    s

--- report/management/commands/import_report.py
+++ report/management/commands/import_report.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from report.models import BillAgeingReport
 from custom.classes import Ikea
 from report.models import PartyReport
@@ -12,9 +13,10 @@ today = datetime.now().date()
 for company in Company.objects.all() :
     print("Running sync for " + company.name)
     i = Ikea(company.pk)
-    BeatReport.update_db(i,company,EmptyArgs())
-    BillAgeingReport.update_db(i,company,EmptyArgs())
-    PartyReport.update_db(i,company,EmptyArgs())
+    # BeatReport.update_db(i,company,EmptyArgs())
+    # BillAgeingReport.update_db(i,company,EmptyArgs())
+    # PartyReport.update_db(i,company,EmptyArgs())
+    SalesRegisterReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(months=3),tod=today))
 
 # CollectionReport.update_db(i,company,DateRangeArgs(fromd=today,tod=today))
 # SalesRegisterReport.update_db(i,company,DateRangeArgs(fromd=today,tod=today))
