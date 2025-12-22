e80d50b4daad1425eafcaaa86c754f2f55f2d93e
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-12-23 00:33:05 +0530

    s

--- bill/urls.py
+++ bill/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path("order/", views.manage_order, name="manage_order"),
     path("party_credit/", views.party_credit, name="party_credit"),
     path("get_billing_stats/", views.get_billing_stats, name="get_billing_stats"),
+    path("stop_billing/", views.billing_stop, name="billing_stop"),
     path('', include(router.urls)),
 ]
