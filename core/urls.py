7315ce0605090d4ed48bb8257a3253d9cd39ffc6
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2026-01-20 18:40:14 +0530

    s

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("login", TokenObtainPairView.as_view(), name="auth-login"),
-    path("logout", auth_logout, name="auth-logout"),
-    path("me", auth_me, name="auth-me"),
     path("usersession", usersession_update, name="usersession"),
     path("companies", get_companies, name="companies"),
 ]
