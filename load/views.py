efb309542b6086a4eb524343ab590322abeb9245
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2026-01-24 13:48:18 +0530

    s

--- load/views.py
+++ load/views.py
@@ -151,17 +151,26 @@ def download_load_summary(request) :
     df = pd.merge(df, product_master[["sku","desc"]] , on="sku", how="left") 
     df = df[["cbu","sku","desc","mrp","purchase_qty","load_qty","diff"]]
     mismatch = df[df["diff"] != 0]
-    cbu_diff_map = mismatch.groupby("cbu")["diff"].sum().to_dict()
-    mismatch["cbu_diff"] = mismatch["cbu"].replace(cbu_diff_map)
-    mismatch_mrp = mismatch[mismatch["cbu_diff"] == 0]
-    mismatch_cbu = mismatch[mismatch["cbu_diff"] != 0]
+
+    mismatch["diff_value"] = mismatch["diff"] * mismatch["mrp"]
+    cbu_diff_qty_map = mismatch.groupby("cbu")["diff"].sum().to_dict()
+    cbu_diff_value_map = mismatch.groupby("cbu")["diff_value"].sum().to_dict()
+    
+    mismatch["cbu_diff_qty"] = mismatch["cbu"].replace(cbu_diff_qty_map)
+    mismatch["cbu_diff_value"] = mismatch["cbu"].replace(cbu_diff_value_map)
+    mismatch_higher_mrp = mismatch[(mismatch["cbu_diff_value"] > 0) & (mismatch["cbu_diff_qty"] == 0)]
+    mismatch_lower_mrp = mismatch[(mismatch["cbu_diff_value"] < 0) & (mismatch["cbu_diff_qty"] == 0)]
+    mismatch_cbu = mismatch[mismatch["cbu_diff_qty"] != 0]
+    del mismatch["diff_value"] , mismatch["cbu_diff_value"]
+
     correct = df[df["diff"] == 0]
     user_dir = os.path.join(settings.MEDIA_ROOT, "load", request.user.pk)
     os.makedirs(user_dir, exist_ok=True)
     file_path = os.path.join(user_dir, f"load_summary_{load.id}.xlsx")
     with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
         mismatch_cbu.to_excel(writer, index=False, sheet_name='Mismatch (CBU)')
-        mismatch_mrp.to_excel(writer, index=False, sheet_name='Mismatch (MRP)')
+        mismatch_higher_mrp.to_excel(writer, index=False, sheet_name='Mismatch (Higher MRP)')
+        mismatch_lower_mrp.to_excel(writer, index=False, sheet_name='Mismatch (Lower MRP)')
         mismatch.to_excel(writer, index=False, sheet_name='Mismatch')
         correct.to_excel(writer, index=False, sheet_name='Correct')
         box_summary.to_excel(writer, index=False, sheet_name='Box')
