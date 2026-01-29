3c166f999c226ae7bd5705d807a0108271bed7e1
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2026-01-29 12:53:42 +0530

    s

--- report/management/commands/import_report.py
+++ report/management/commands/import_report.py
@@ -11,14 +11,16 @@ from core.models import Company
 from report.models import CollectionReport
 from datetime import datetime
 import datetime
+from bill.models import Bill
 # today = datetime.now().date()
 #TODO: make it parameterized
 for company in Company.objects.all() :
     print("Running sync for " + company.name)
     i = Ikea(company.pk)
     today = datetime.date.today() 
-    # SalesRegisterReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(months=7),tod=today))
-    CollectionReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(months=3),tod=today))
+    SalesRegisterReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(days=1),tod=today))
+    Bill.sync_with_salesregister(company,fromd=today-relativedelta(days=1),tod=today)
+    # CollectionReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(months=3),tod=today))
     # # SalesRegisterReport.update_db(i,company,DateRangeArgs(fromd=today-relativedelta(months=7),tod=today))
     # OutstandingReport.update_db(i,company,EmptyArgs())
     # BeatReport.update_db(i,company,EmptyArgs())
